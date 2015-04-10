1a33609a4273a00404bc12130dee48ecb314dae6
Ota Mares <o.mares@rebuy.de>
2015-04-10 16:39:02 +0200

    Fix class name reference

--- Rb.Forms.Barcode.Droid/View/CameraService.cs
+++ Rb.Forms.Barcode.Droid/View/CameraService.cs
@@ -14,10 +14,10 @@ namespace Rb.Forms.Barcode.Droid.View
 
         private readonly ScannerCamera scannerCamera;
         private readonly AutoFocusCallback autoFocus;
-        private readonly RbOptions options;
+        private readonly RbConfig options;
         private readonly CameraConfigurator cameraConfigurator;
 
-        public CameraService(BarcodeScanner renderer, ScannerCamera scannerCamera, RbOptions options)
+        public CameraService(BarcodeScanner renderer, ScannerCamera scannerCamera, RbConfig options)
         {
             this.options = options;
             this.renderer = renderer;
