205ac2593f8139078dd1e27ff7e34d34b3798499
Ota Mares <o.mares@rebuy.de>
2015-04-27 15:12:28 +0200

    Sample goes XAML

--- Sample/Sample.Droid/Resources/Resource.designer.cs
+++ Sample/Sample.Droid/Resources/Resource.designer.cs
@@ -55,6 +55,9 @@ namespace Sample.Droid
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int slideout = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
