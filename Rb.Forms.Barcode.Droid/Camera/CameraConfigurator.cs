f33a7a4cab307a1e332b0a8d5360b81303132e79
Denis Zakharov <exxbrain@mail.ru>
2017-12-12 02:54:53 +0300

    Update to Xamarin.Forms 2.5.0.121934, Xamarin.GooglePlayServices 60.1142.0

--- Rb.Forms.Barcode.Droid/Camera/CameraConfigurator.cs
+++ Rb.Forms.Barcode.Droid/Camera/CameraConfigurator.cs
@@ -242,7 +242,7 @@ namespace Rb.Forms.Barcode.Droid.Camera
         /// </summary>
         private string determineSceneMode(AndroidCamera.Parameters parameters)
         {
-            if (parameters.SupportedSceneModes.Contains(AndroidCamera.Parameters.SceneModeBarcode)) {
+            if (parameters.SupportedSceneModes?.Contains(AndroidCamera.Parameters.SceneModeBarcode) ?? false) {
                 return AndroidCamera.Parameters.SceneModeBarcode;
             }
 
